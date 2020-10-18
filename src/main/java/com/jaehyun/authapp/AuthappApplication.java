6f9ef7f7f8d7a480333d8d4d8500e7cf7bb77edf
Parkjaehyun <pjh3909@gmail.com>
2020-10-18 21:06:10 +0900

    project init

--- src/main/java/com/jaehyun/authapp/AuthappApplication.java
+++ src/main/java/com/jaehyun/authapp/AuthappApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthappApplication {
 
 	public static void main(String[] args) {
-		/*TEST */
+		/*TEST update*/
 		SpringApplication.run(AuthappApplication.class, args);
 	}
 
