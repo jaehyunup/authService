1b4e0953c9f835532390ada9a68a4bbb084b2812
Parkjaehyun <pjh3909@gmail.com>
2020-10-20 14:01:48 +0900

    circle ci update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# authService
+# [AuthService](https://github.com/jaehyunup/authservice) &middot;
+
 사용자 인증을 위한 토큰 발급/인증 서비스 서버
--- src/main/java/com/jaehyun/authapp/AuthappApplication.java
+++ src/main/java/com/jaehyun/authapp/AuthappApplication.java
@@ -3,6 +3,17 @@ package com.jaehyun.authapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+* @packageName : com.jaehyun.authapp
+* @fileName	   : AuthappApplication.java
+* @author 	   : parkjaehyun
+* @date 	   : 2020.10.20
+* @description :
+* ============================================================================
+* DATE       AUTHOR         NOTE
+* ----------------------------------------------------------------------------
+* 2020.10.20       parkjaehyun           최초생성
+*/ 
 @SpringBootApplication
 public class AuthappApplication {
 
