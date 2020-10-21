b625ef93a9cff34a36c2f6840c5a16417a8ab801
Parkjaehyun <pjh3909@gmail.com>
2020-10-21 16:43:27 +0900

    정크코드 제거

--- src/main/java/com/jaehyun/authapp/AuthappApplication.java
+++ src/main/java/com/jaehyun/authapp/AuthappApplication.java
@@ -1,14 +1,7 @@
 package com.jaehyun.authapp;
 
-import javax.sql.DataSource;
-
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 /**
 * @packageName : com.jaehyun.authapp
